f099a6cf7e46efda11eb86eb6b9dadc58c505e3f
Oleksii Larkin <oleksii.larkin@gmail.com>
2022-02-04 15:46:42 +0100

    new homework

---
+++ skutarenko_01.swift
@@ -0,0 +1,6 @@
+let firstName = "Oleksii"
+let lastName = "Larkin"
+let age = 31
+let height = 191
+let weight = 96
+print("My name is \(firstName) \(lastName). I'm \(age) years old. My height \(height) cm. My weight \(weight) kg.")
